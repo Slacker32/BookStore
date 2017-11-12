6ae47c352b481207c8e1054ed5b298a41e5a547e
Slacker <slack32@mail.ru>
2017-11-13 00:45:32 +0200

    add booksShopSite project

---
+++ BooksShopSite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksShopSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        BooksShopCore.BooksShop bookShop = new BooksShopCore.BooksShop();
+        public ActionResult Index()
+        {
+            var bookList = bookShop.Books.ShowAllBooks("rus", "byn");
+            ViewBag.Books = bookList;
+
+            return View();
+        }
+
+    }
+}
